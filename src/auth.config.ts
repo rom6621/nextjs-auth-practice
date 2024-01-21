6e429c80c3b6d586384ed111489b150442c822c0
rom6621 <3103rom@gmail.com>
2024-01-21 13:38:05 +0900

    feat: NextAuthを設定

---
+++ src/auth.config.ts
@@ -0,0 +1,38 @@
+import Credentials from "next-auth/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+
+import { signInSchema } from "@/schemas";
+import { compare } from "bcrypt";
+import { db } from "@/lib/db";
+
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials, request) {
+        const validatedFields = signInSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await db.user.findUnique({
+            where: {
+              email,
+            },
+          });
+
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const passwordMatch = await compare(password, user.password);
+
+          if (!passwordMatch) {
+            return user;
+          }
+        }
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { auth } from "@/auth";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute && isLoggedIn) {
+    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/signup", nextUrl));
+  }
+
+  return null;
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
