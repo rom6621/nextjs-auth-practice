1e9f614b5bbfea54dd3b2abe6f2e0973dbe952a1
rom6621 <3103rom@gmail.com>
2024-01-21 16:58:24 +0900

    feat: SignOutを可能に

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,5 +1,26 @@
-const Home = () => {
-  return <main />;
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.table(session);
+
+  return (
+    <main>
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}
+      >
+        <Button variant="ghost" type="submit">
+          Sign Out
+        </Button>
+      </form>
+    </main>
+  );
 };
 
 export default Home;
