1c008953e69d5692691d4ffeb3f0193feb722db7
rom6621 <3103rom@gmail.com>
2024-01-21 15:47:58 +0900

    fix: middlewareを修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
-import { auth } from "@/auth";
+import authConfig from "@/auth.config";
 import {
   DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
   authRoutes,
   publicRoutes,
 } from "@/routes";
+import NextAuth from "next-auth";
+
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   const { nextUrl } = req;
