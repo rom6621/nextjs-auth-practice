7bcac28ea939ee6a57ebdf2efd08c3c13e1b4398
rom6621 <3103rom@gmail.com>
2024-01-21 11:24:20 +0900

    feat: 必須項目のエラーメッセージを追加

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,14 +1,22 @@
 import { z } from "zod";
 
 export const signUpSchema = z.object({
-  email: z.string().email({
-    message: "メールアドレスは必須です。",
-  }),
-  password: z.string().min(6, {
-    message: "パスワードは6文字以上です。",
-  }),
-  nickname: z.string().min(1, {
-    message: "ニックネームは必須です。",
+  email: z
+    .string({
+      required_error: "メールアドレスは必須です。",
+    })
+    .email({
+      message: "メールアドレスの形式が異なります。",
+    }),
+  password: z
+    .string({
+      required_error: "パスワードは必須です。",
+    })
+    .min(6, {
+      message: "パスワードは6文字以上です。",
+    }),
+  nickname: z.string({
+    required_error: "ニックネームは必須です。",
   }),
 });
 
